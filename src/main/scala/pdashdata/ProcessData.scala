726df66730e5cdb25179a0a751ba47bed028afc2
Furuta Naoki <fn.nk.2002@gmail.com>
2024-09-23 19:20:24 +0900

    課題ごとの総LOCを取得する関数

--- src/main/scala/pdashdata/ProcessData.scala
+++ src/main/scala/pdashdata/ProcessData.scala
@@ -1,10 +1,19 @@
 package pdashdata
 
 import java.util.Map
+import java.util.HashMap
 import java.util.List
 
 class ProcessData(var timeLogs: List[TimeLog], var defectLogs: Map[Int, List[DefectLog]], var programDatas: Map[Int, Map[String, Any]]) {
     def getTimeLogs(): List[TimeLog] = timeLogs
     def getDefectLogs(): Map[Int, List[DefectLog]] = defectLogs
     def getProgramDatas(): Map[Int, Map[String, Any]] = programDatas
+    
+    def getTotalLOCs(): Map[Int, Int] = {
+        var totalLOCs = new HashMap[Int, Int]()
+        for (i <- 1 to 8) {
+            totalLOCs.put(i, programDatas.get(i).get("Total LOC").asInstanceOf[Double].toInt)
+        }
+        totalLOCs
+    }
 }
