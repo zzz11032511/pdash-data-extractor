8ba86a17adfdddd89170fafd9a5cbfbb2fb89b0b
Furuta Naoki <fn.nk.2002@gmail.com>
2024-09-28 20:49:47 +0900

    部品関連のクラス取得処理を移動

--- src/main/scala/pdashdata/PDashDataExtractor.scala
+++ src/main/scala/pdashdata/PDashDataExtractor.scala
@@ -75,11 +75,106 @@ class PDashDataExtractor {
             }
         } catch {
             case e: Exception => println(e)
+            return null
         }
 
-        val a = DataFileParser.loadBaseParts(programDatas(4))
+        val a = loadBaseParts(programDatas(4))
         a.foreach(println)
 
+        val b = loadAdditionalParts(programDatas(4))
+        b.foreach(println)
+
+        val c = loadReusedParts(programDatas(6))
+        c.foreach(println)
+
         new ProcessData(timeLogs, defectLogs, programDatas)
+    }
+
+    private def loadBaseParts(programData: Map[String, Any]): List[BaseParts] = {
+        var baseParts = List[BaseParts]()
+
+        programData.get("Base_Parts_List")
+                   .getOrElse(List.empty[String])
+                   .asInstanceOf[List[String]]
+                   .foreach((programNum: String) => {
+            val name = getStringValue(programData, s"Base_Parts/$programNum/Description")
+            val estBase = getDoubleValue(programData, s"Base_Parts/$programNum/Base")
+            val estAdded = getDoubleValue(programData, s"Base_Parts/$programNum/Added")
+            val estModified = getDoubleValue(programData, s"Base_Parts/$programNum/Modified")
+            val estDeleted = getDoubleValue(programData, s"Base_Parts/$programNum/Deleted")
+            val actBase = getDoubleValue(programData, s"Base_Parts/$programNum/Actual Base")
+            val actAdded = getDoubleValue(programData, s"Base_Parts/$programNum/Actual Added")
+            val actModified = getDoubleValue(programData, s"Base_Parts/$programNum/Actual Modified")
+            val actDeleted = getDoubleValue(programData, s"Base_Parts/$programNum/Actual Deleted")
+
+            val basePart = new BaseParts(
+                name, estBase, estAdded, estModified, estDeleted,
+                actBase, actAdded, actModified, actDeleted
+            )
+
+            baseParts = baseParts :+ basePart
+        })
+
+        baseParts
+    }
+
+    private def loadAdditionalParts(programData: Map[String, Any]): List[AdditionalParts] = {
+        var additionalParts = List[AdditionalParts]()
+
+        programData.get("New_Objects_List")
+                   .getOrElse(List.empty[String])
+                   .asInstanceOf[List[String]]
+                   .foreach((programNum: String) => {
+            val name = getStringValue(programData, s"New Objects/$programNum/Description")
+            val estSize = getDoubleValue(programData, s"New Objects/$programNum/LOC")
+            val actSize = getDoubleValue(programData, s"New Objects/$programNum/Actual LOC")
+            val relativeSize = getStringValue(programData, s"New Objects/$programNum/Relative Size")
+            val estNumOfMethod = getIntValue(programData, s"New Objects/$programNum/Methods")
+            val estIsNewReused = getDoubleValue(programData, s"New Objects/$programNum/New Reused?") == 1.0
+            val actNumOfMethod = getIntValue(programData, s"New Objects/$programNum/Actual Methods")
+            val actIsNewReused = getDoubleValue(programData, s"New Objects/$programNum/Actual New Reused?") == 1.0
+            val methodType = getStringValue(programData, s"New Objects/$programNum/Type")
+
+            val additionalPart = new AdditionalParts(
+                name, estSize, actSize, relativeSize,
+                estNumOfMethod, estIsNewReused,
+                actNumOfMethod, actIsNewReused,
+                methodType)
+
+            additionalParts = additionalParts :+ additionalPart
+        })
+
+        additionalParts
+    }
+
+    private def loadReusedParts(programData: Map[String, Any]): List[ReusedParts] = {
+        var reusedParts = List[ReusedParts]()
+
+        programData.get("Reused_Objects_List")
+                   .getOrElse(List.empty[String])
+                   .asInstanceOf[List[String]]
+                   .foreach((programNum: String) => {
+            val name = getStringValue(programData, s"Reused Objects/$programNum/Description")
+            val estSize = getDoubleValue(programData, s"Reused Objects/$programNum/LOC")
+            val actSize = getDoubleValue(programData, s"Reused Objects/$programNum/Actual LOC")
+
+            val reusedPart = new ReusedParts(name, estSize, actSize)
+
+            reusedParts = reusedParts :+ reusedPart
+        })
+
+        reusedParts
+    }
+
+    private def getStringValue(programData: Map[String, Any], key: String): String = {
+        programData.get(key).getOrElse("").asInstanceOf[String]
+    }
+
+    private def getDoubleValue(programData: Map[String, Any], key: String): Double = {
+        programData.get(key).getOrElse(0.0).asInstanceOf[Double]
+    }
+
+    private def getIntValue(programData: Map[String, Any], key: String): Int = {
+        getDoubleValue(programData, key).toInt
     }
 }
